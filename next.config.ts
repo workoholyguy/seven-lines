42b21476031693b9aadf4a765c4a521e1fb5e6b3
Omar Madjitov <95881196+workoholyguy@users.noreply.github.com>
2025-08-05 16:12:46 -0400

    Improved Recent Projects

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    // Fix for lightningcss issue on Netlify
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
